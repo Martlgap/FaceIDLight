7e5c21d2f5b8b3422753478d9b0c60fcf7cbc1a0
Martin Knoche <Martin.Knoche@tum.de>
2024-07-02 20:00:58 +0200

    made tests more robust

--- tests/test_CI.py
+++ tests/test_CI.py
@@ -8,15 +8,16 @@ import json
 @pytest.mark.face_detection
 def test_single_face():
     detector = FaceDetection()
-    img = cv2.imread('tests/data/img1.png')
+    img = cv2.imread("tests/data/img1.png")
     detection = detector.detect_faces(img)
-    assert (json.load(open('tests/data/detections.json', 'r'))['img1'] == detection[0][1]).all()
+    target_detection = json.load(open("tests/data/detections.json", "r"))["img1"]
+    assert abs(sum(sum(np.asarray(detection[0][1]) - np.asarray(target_detection)))) < 1e-5
 
 
 @pytest.mark.face_detection
 def test_multi_face():
     detector = FaceDetection()
-    img = cv2.imread('tests/data/img3.png')
+    img = cv2.imread("tests/data/img3.png")
     detection = detector.detect_faces(img)
     assert len(detection) == 2
 
@@ -24,7 +25,7 @@ def test_multi_face():
 @pytest.mark.face_detection
 def test_no_face():
     detector = FaceDetection()
-    img = cv2.imread('tests/data/img2.png')
+    img = cv2.imread("tests/data/img2.png")
     detection = detector.detect_faces(img)
     assert not detection
 
@@ -32,16 +33,18 @@ def test_no_face():
 @pytest.mark.face_alignment
 def test_align():
     detector = FaceDetection()
-    img = cv2.imread('tests/data/img1.png')
-    detection = json.load(open('tests/data/detections.json', 'r'))['img1']
+    img = cv2.imread("tests/data/img1.png")
+    detection = json.load(open("tests/data/detections.json", "r"))["img1"]
     face = detector.get_face(img, np.asarray(detection))
-    face_target = cv2.imread('tests/data/face.png')
-    assert (face == face_target).all()
+    face_target = cv2.imread("tests/data/face.png")
+    print(sum(sum(sum(face - face_target))))
+    assert abs(sum(sum(sum(face - face_target)))) < 5
 
 
 @pytest.mark.face_recognition
 def test_recognition():
     recognizer = FaceRecognition()
-    face = cv2.imread('tests/data/face.png')
+    face = cv2.imread("tests/data/face.png")
     emb = recognizer.get_emb(face[None])
-    assert (emb[0] == json.load(open('tests/data/detections.json', 'r'))['face_emb'][0]).all()
+    target_emb = json.load(open("tests/data/detections.json", "r"))["face_emb"]
+    assert abs(sum(sum(np.asarray(emb[0]) - np.asarray(target_emb[0])))) < 0.2
