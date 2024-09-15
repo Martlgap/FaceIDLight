599b7132f70ef1ffc88246eeaf447c382e5f6e2e
Martin Knoche <Martin.Knoche@tum.de>
2024-09-15 19:39:26 +0200

    fixed setup

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from setuptools import setup, find_packages
 
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
 
 setup(
     version="0.1",
@@ -11,15 +13,5 @@ setup(
     license="MIT",
     description="A lightweight face-recognition toolbox.",
     long_description=open("README.md").read(),
-    install_requires=[
-        "setuptools>=70.2.0,<70.3",
-        "opencv-python>=4.5.1.48,<4.10.1.0",
-        "numpy>=2.0.0,<2.1.0",
-        "tqdm>=4.59,<4.67",
-        "scikit-image>=0.17.2,<0.25.0",
-        "matplotlib>=3.3.3,<3.10.0",
-        "scipy>=1.4.1,<1.15.0",
-        "scikit-learn>=0.24,<1.6",
-        "tflite-runtime==2.14.0",
-    ],
+    install_requires=requirements,
 )
